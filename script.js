08d4e78a8b87eeb6320249733a1512facb228409
seannovela1 <seannovela5484@my.whccd.edu>
2024-10-22 13:59:36 -0700

    committed "script.js"

---
+++ script.js
@@ -0,0 +1,10 @@
+avascript
+console.log(8 * null); // This will output 0
+console.log("5" - 1); // This will output 4
+console.log("5" + 1); // This will output "51"
+console.log("five" * 2); // This will output NaN (Not a Number)
+console.log(false == 0); // This will output true
+console.log(5 > 2 && 6 > 3); // This will output true
+console.log(false == false ? "equal" : "not equal"); // This will output "equal"
+console.log(true || false); // This will output true
+
